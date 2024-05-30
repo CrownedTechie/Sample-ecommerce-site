e2c4eb7cc98a965969208352b9e47ab6dad2bd28
CrownedTechie <nenchi00@gmail.com>
2024-05-30 22:36:25 +0100

    refactor code in the footer component to reduce redundancy

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,16 @@ import DisplayImg from './DisplayImg';
 import facebookBlue from '../assets/facebook-blue.png';
 import instagramBlue from '../assets/instagram-blue.png';
 import twitterBlue from '../assets/twitter-blue.png';
-import IconBtn from './IconBtn';
+import { FOOTER_LINKS } from '../data';
 
+const FooterList = ({title, items}) => (
+    <ul>
+        <h4>{title}</h4>
+        {items.map((item, index) => (
+            <li key={index}> { item } </li>
+        ))}
+    </ul>
+);
 
 const Footer = () => {
   return (
@@ -21,49 +29,20 @@ const Footer = () => {
               </div>
 
               <div className='footer-center'>
-                  <ul>
-                      <h4>Company Info</h4>
-                      <li>About us</li>
-                      <li>Carrier</li>
-                      <li>We are hiring</li>
-                      <li>Blog</li>
-                  </ul>
+                {FOOTER_LINKS.map((link, index) => (
+                        <FooterList key={index} title={link.title} items={link.items} />
+                ))}
 
-                  <ul>
-                      <h4>Legal</h4>
-                      <li>About us</li>
-                      <li>Carrier</li>
-                      <li>We are hiring</li>
-                      <li>Blog</li>
-                  </ul>
+                <ul>
+                    <h4>Get in Touch</h4>
 
-                  <ul>
-                      <h4>Features</h4>
-                      <li>Business Marketing</li>
-                      <li>User Analytics</li>
-                      <li>Live Chat</li>
-                      <li>Unlimited Support</li>
-                  </ul>
-
-                  
-                  <ul>
-                      <h4>Resources</h4>
-                      <li>IOS & Android</li>
-                      <li>Watch a Demo</li>
-                      <li>Customers</li>
-                      <li>API</li>
-                  </ul>
-
-                  <ul>
-                      <h4>Get in Touch</h4>
-
-                      <div className="input-box">
+                    <div className="input-box">
                         <input type="email" name="" id="" placeholder='Your Email'/>
                         <button className='getInTouch-btn'>Subscribe</button>
-                      </div>
+                    </div>
 
-                      <p>Lorem, ipsum dolor sit amet</p>
-                  </ul>
+                    <p>Lorem, ipsum dolor sit amet</p>
+                </ul>
               </div>
 
               <div className='footer-bottom'>
