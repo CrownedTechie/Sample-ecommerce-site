c0075648fd21eb579f6202ef02b07d8de85eda49
CrownedTechie <nenchi00@gmail.com>
2024-06-01 11:40:51 +0100

    style the mobile design for the call-to-action page

--- src/components/CallToAction.jsx
+++ src/components/CallToAction.jsx
@@ -6,10 +6,14 @@ const CallToAction = () => {
       <>
           <section>
             <div className="CallToAction-heading">
-                <h6>Designing Better Experience</h6>
-                <h2>Problems trying to resolve the conflict between </h2>
+              <h6>Designing Better Experience</h6>
+              <h2>Problems trying to resolve the conflict between </h2>
+          
+              <div>
                 <p>Problems trying to resolve the conflict between the two major realms of Classical physics: </p>
                 <h3>$16.48</h3>
+              </div>
+               
                 <IconBtn value='add your call to action' styling='CallToAction-btn' />
             </div>
           </section>
