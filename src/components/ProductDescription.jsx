e4e66a6a5242a21e0984f8afd07f3811e152836e
CrownedTechie <nenchi00@gmail.com>
2024-06-02 17:24:09 +0100

    style the desktop design for the product description component

--- src/components/ProductDescription.jsx
+++ src/components/ProductDescription.jsx
@@ -1,13 +1,36 @@
 import TabButton from "./TabButton";
+import '../stylings/ProductDescription.css';
+import DisplayImg from "./DisplayImg";
+import lamp from "../assets/graphic-design1.png";
 
 const ProductDescription = () => {
     return (
         <>
-            <menu>
-                <TabButton>description</TabButton>
-                <TabButton>additional information</TabButton>
-                <TabButton>reviews(0)</TabButton>
-            </menu>
+            <setion className="productdescription-section">
+                <menu>
+                    <TabButton>description</TabButton>
+                    <TabButton>additional information</TabButton>
+                    <TabButton>reviews(0)</TabButton>
+                </menu>
+                <hr />
+
+
+                <article>
+                    <div className="extraContent-col">
+                        <div className="extra-content">
+                            <h3>the quick fox jumps over </h3>
+
+                            <p>Met minim Mollie non desert Alamo est sit cliquey dolor do met sent. RELIT official consequent door ENIM RELIT Mollie. Excitation venial consequent sent nostrum met.</p>
+
+                            <blockquote>Met minim Mollie non desert Alamo est sit cliquey dolor do met sent. RELIT official consequent door ENIM RELIT Mollie. Excitation venial consequent sent nostrum met.</blockquote>
+
+                            <p>Met minim Mollie non desert Alamo est sit cliquey dolor do met sent. RELIT official consequent door ENIM RELIT Mollie. Excitation venial consequent sent nostrum met.</p>
+                        </div>
+                        <DisplayImg img={lamp} styling='extraContent-img' />
+                    </div>
+                   
+                </article>
+            </setion>
         </>
     )
 }
