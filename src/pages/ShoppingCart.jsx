c24e44856ce3605a096a8659324895894875cb45
CrownedTechie <nenchi00@gmail.com>
2024-06-03 21:09:10 +0100

    style bestseller products on shopping cart page

--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -1,6 +1,8 @@
 import AlertHeader from "../components/AlertHeader";
 import Header from "../components/Header";
 import CartBox from "../components/CartBox";
+import ProductCard from "../components/ProductCard";
+import lamp from "../assets/graphic-design1.png";
 
 
 
@@ -12,6 +14,30 @@ const ShoppingCart = () => {
             <Header />
 
             <CartBox />
+            
+            <section>
+                <h2> products related to items in your cart </h2>
+                <hr />
+
+                <div>
+                    <div>
+                        {/* REDUNDANT WILL BE CHANGED LATER */}
+                        <ProductCard img={lamp} />
+                        <ProductCard img={lamp}/>
+                        <ProductCard img={lamp}/>
+                        <ProductCard img={lamp}/>
+                        <ProductCard img={lamp}/>
+                        <ProductCard img={lamp}/>
+                        <ProductCard img={lamp}/>
+                        <ProductCard img={lamp}/>
+                    </div>
+                </div>
+
+
+            </section>
+           
+
+
         </>
     )
 };
